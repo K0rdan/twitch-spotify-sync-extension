7edbc21cc3c535b1cc0604082ead06eb7322d881
Benjamin Christau <benjamin.christau@oyez.fr>
2020-12-01 14:41:28 +0100

    test panel page

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { NextPage } from 'next';
-
-type HomeProps = {};
-export const HomePage: NextPage<HomeProps> = ({}: HomeProps) => <div>TEST</div>;
-
-export default HomePage;
---
+++ src/pages/panel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+type PanelProps = {};
+export const PanelPage: NextPage<PanelProps> = ({}: PanelProps) => (
+  <div>TEST</div>
+);
+
+export default PanelPage;
